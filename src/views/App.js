fb3624a735d011d42c9bdefa890f9b021f24846c
PhucChiVas161 <vuatrochoi159@gmail.com>
2023-03-22 15:05:28 +0700

    Update React vs CSS

--- src/views/App.js
+++ src/views/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.scss';
+import logo from "./logo.svg";
+import "./App.scss";
+import Nav from "./Nav";
 
 function App() {
+  let name = "Phúc sẽ cố gắng";
+  let link = "https://facebook.com";
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Reactjs khó VCL nhưng {name}</p>
+        <a href={link}>Click Me !!!</a>
       </header>
     </div>
   );
